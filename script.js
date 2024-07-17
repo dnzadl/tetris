96b7cdb266da92e60e7c57be61c044fd61100a96
dnzadl <denizadlig@hotmail.com>
2024-07-17 12:54:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const highScoresList = document.getElementById('highScores');
     const gameOverMessage = document.getElementById('gameOverMessage');
     const restartButton = document.getElementById('restartButton');
+    const blockBreakSound = document.getElementById('blockBreakSound');
+    const winSound = document.getElementById('winSound');
+    const loseSound = document.getElementById('loseSound');
 
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
@@ -93,6 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
             updateScore();
             if (score >= 2000) {
                 gameOver = true;
+                winSound.play();
                 gameOverMessage.textContent = 'Helal Sana Beah!';
                 gameOverMessage.style.display = 'block';
                 restartButton.style.display = 'block';
@@ -102,6 +106,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 piece = randomPiece();
                 if (collide(grid, piece)) {
                     gameOver = true;
+                    loseSound.play();
                     gameOverMessage.textContent = 'Başaramadın!';
                     gameOverMessage.style.display = 'block';
                     restartButton.style.display = 'block';
@@ -161,6 +166,9 @@ document.addEventListener('DOMContentLoaded', () => {
             lines++;
             y++;
         }
+        if (lines > 0) {
+            blockBreakSound.play();
+        }
         return lines;
     }
 
@@ -207,8 +215,8 @@ document.addEventListener('DOMContentLoaded', () => {
             dropPiece();
             drawGrid();
             drawBlock(piece.x, piece.y, piece.shape, piece.color);
-            setTimeout(update, 500);
         }
+        setTimeout(update, 500); // Oyun döngüsünü devam ettir
     }
 
     document.addEventListener('keydown', (event) => {
